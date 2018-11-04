7a5815ce233d30bcf00810be8cd620df379cd66f
Marius Brad <mbrad26@gmail.com>
2018-11-04 15:03:07 +0000

    ..

--- one_hot_encoding.py
+++ one_hot_encoding.py
@@ -1,5 +1,6 @@
 import numpy as np
 import string
+from keras.preprocessing.text import Tokenizer
 
 samples = ['The cat sat on the mat', 'The dog ate my homework']
 
@@ -44,3 +45,51 @@ for i, sample in enumerate(samples):
 print(f'Characters {characters}')
 print(f'Results {len(results)}')
 print(f'Token Index {token_index}')
+
+########################################################################
+
+tokenizer = Tokenizer(num_words=1000)
+tokenizer.fit_on_texts(samples)
+
+sequences = tokenizer.texts_to_sequences(samples)
+print('Sequences', sequences)
+
+one_hot_results = tokenizer.texts_to_matrix(samples, mode='binary')
+print('One hot results', one_hot_results)
+
+word_index = tokenizer.word_index
+print('Word index', word_index)
+
+##########################################################################
+
+dim = 1000
+max_length = 10
+
+results = np.zeros((len(samples), max_length, dim))
+
+for i, sample in enumerate(samples):
+    for j, word in list(enumerate(sample.split())):
+        index = abs(hash(word)) % dim
+        results[i, j, index] = 1
+
+print(f'Results: {results}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
