a441b4e4ebd6b27ca34f325405cdfa94774274ea
Marius Brad <mbrad26@gmail.com>
2018-11-05 17:36:45 +0000

    ...

--- pretrained_word_embeddings.py
+++ pretrained_word_embeddings.py
@@ -47,3 +47,26 @@ x_train = data[:train_sample]
 y_train = labels[:train_sample]
 x_val = data[train_sample: train_sample + val_sample]
 y_val = labels[train_sample: train_sample + val_sample]
+
+
+glove_dir = 'C:\/Users\mbrad\Downloads\kaggle\glove'
+
+embeddings_index = {}
+f = open(os.path.join(glove_dir, 'glove.6B.100d.txt'), encoding='utf8')
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
+
+embedding_dim = 100
+
+embedding_matrix = np.zeros((max_words, embedding_dim))
+for word, i in word_index.items():
+    if i < max_words:
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[i] = embedding_vector
+
+print('Found {}s word vectors'.format(len(embeddings_index)))
