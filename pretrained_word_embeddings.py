da64ccaf234cff5fb469e35ccf1996cf643da874
Marius Brad <mbrad26@gmail.com>
2018-11-06 11:46:46 +0000

    ...

--- pretrained_word_embeddings.py
+++ pretrained_word_embeddings.py
@@ -1,26 +1,58 @@
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Embedding, Flatten, Dense
 
 
-base_dir = 'C:\/Users\mbrad\Downloads\kaggle\/aclImdb'
-train_dir = os.path.join(base_dir, 'train')
+imdb_dir = 'C:\/Users\mbrad\Downloads\kaggle\/aclImdb'
+train_dir = os.path.join(imdb_dir, 'train')
+test_dir = os.path.join(imdb_dir, 'test')
 
-texts = []
-labels = []
+train_texts = []
+train_labels = []
 
 for label_type in ['pos', 'neg']:
     dir_name = os.path.join(train_dir, label_type)
     for fname in os.listdir(dir_name):
         if fname[-4:] == '.txt':
             f = open(os.path.join(dir_name, fname), encoding='utf8')
-            texts.append(f.read())
+            train_texts.append(f.read())
             f.close()
             if label_type == 'neg':
-                labels.append(0)
+                train_labels.append(0)
             else:
-                labels.append(1)
+                train_labels.append(1)
+
+test_texts = []
+test_labels = []
+
+for label_type in ['pos', 'neg']:
+    dir_name = os.path.join(test_dir, label_type)
+    for fname in os.listdir(dir_name):
+        if fname[-4:] == '.txt':
+            f = open(os.path.join(dir_name, fname), encoding='utf8')
+            test_texts.append(f.read())
+            f.close()
+            if label_type == 'neg':
+                test_labels.append(0)
+            else:
+                test_labels.append(1)
+
+
+glove_dir = 'C:\/Users\mbrad\Downloads\kaggle\glove'
+
+embedding_index = {}
+f = open(os.path.join(glove_dir, 'glove.6B.100d.txt'), encoding='utf8')
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embedding_index[word] = coefs
+f.close()
+print('Found {}s word vectors.'.format(len(embedding_index)))
 
 
 maxlen = 100
@@ -29,39 +61,26 @@ train_samples = 200
 val_samples = 10000
 
 tokenizer = Tokenizer(num_words=max_words)
-tokenizer.fit_on_texts(texts)
+tokenizer.fit_on_texts(train_texts)
 
-sequence = tokenizer.texts_to_sequences(texts)
+sequence = tokenizer.texts_to_sequences(train_texts)
 word_index = tokenizer.word_index
-print('Found {}s unique tokens')
+print('Found {}s unique tokens.'.format(len(word_index)))
 
 data = pad_sequences(sequence, maxlen=maxlen)
-labels = np.asarray(labels)
+train_labels = np.asarray(train_labels)
 print('Data tensor shape: ', data.shape)
-print('Label tensor shape: ', labels.shape)
+print('Train labels tensor shape: ', train_labels.shape)
 
 indices = np.arange(data.shape[0])
 np.random.shuffle(indices)
 data = data[indices]
-labels = labels[indices]
+train_labels = train_labels[indices]
 
-x_train = data[:train_samples]
-y_train = labels[: train_samples]
-x_val = data[train_samples: train_samples + val_samples]
-y_val = labels[train_samples: train_samples + val_samples]
-
-
-glove_dir = 'C:\/Users\mbrad\Downloads\kaggle\glove'
-
-embeddings_index = {}
-f = open(os.path.join(glove_dir, 'glove.6B.100d.txt'), encoding='utf8')
-for line in f:
-    values = line.split()
-    word = values[0]
-    coefs = np.asarray(values[1:], dtype='float32')
-    embeddings_index[word] = coefs
-f.close()
-print('Found {}s word vectors'.format(len(embeddings_index)))
+x_train = train_texts[:train_samples]
+y_train = train_labels[:train_samples]
+x_val = train_texts[train_samples: train_samples + val_samples]
+y_val = train_labels[train_samples: train_samples + val_samples]
 
 
 embedding_dim = 100
@@ -69,10 +88,48 @@ embedding_dim = 100
 embedding_matrix = np.zeros((max_words, embedding_dim))
 for word, i in word_index.items():
     if i < max_words:
-        embedding_vector = embeddings_index.get(word)
+        embedding_vector = embedding_index.get(word)
         if embedding_vector is not None:
             embedding_matrix[i] = embedding_vector
 
+
+model = Sequential()
+model.add(Embedding(max_words, embedding_dim, input_length=maxlen))
+model.add(Flatten())
+model.add(Dense(32, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.summary()
+
+model.layers[0].set_weights([embedding_matrix])
+model.layers[0].trainable = False
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+
+history = model.fit(x_train, y_train,
+                    epochs=10,
+                    batch_size=32,
+                    validation_data=(x_val, y_val),
+                    verbose=2)
+
+model.save_weights('pretrained_word_embeddings.h5')
+
+history_dict = history.history
+loss = history_dict['loss']
+acc = history_dict['acc']
+val_loss = history_dict['val_loss']
+val_acc = history_dict['val_acc']
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, loss, 'bo', label='Loss')
+plt.plot(epochs, val_loss, 'b', label='Val Loss')
+plt.legend()
+plt.figure()
+
+plt.plot(epochs, acc, 'bo', label='Acc')
+plt.plot(epochs, val_acc, 'b', label='Val Acc')
+plt.legend()
+plt.show()
 
 
 
